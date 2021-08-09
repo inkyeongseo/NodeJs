74e0a520704f9cf3544ed20b118f16ca1267bec7
inkyeong <dlsrud0415@naver.com>
2021-08-09 23:51:07 +0900

    node js

---
+++ index.js
@@ -0,0 +1,12 @@
+
+const express = require('express') //express모듈 가져오기
+const app = express()
+const port = 5000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
