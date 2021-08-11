49749ec2f95be4fb263d150827337addce156a4a
inkyeong <dlsrud0415@naver.com>
2021-08-11 22:28:40 +0900

    node js

--- index.js
+++ index.js
@@ -3,14 +3,6 @@ const express = require('express') //express모듈 가져오기
 const app = express()
 const port = 5000
 
-const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://inkyeong:inkyeong@cluster0.msxuf.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',{
-  useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify:false
-}).then(() => console.log('MongoDB Connected..'))
-  .catch(err => console.log(err))
-
-
-
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
