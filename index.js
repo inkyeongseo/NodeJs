85137fb8bd3596464982ea849072ca09bc715277
inkyeong <dlsrud0415@naver.com>
2021-08-13 15:16:55 +0900

    Nodejs

--- index.js
+++ index.js
@@ -1,17 +1,6 @@
-
 const express = require('express') //express모듈 가져오기
 const app = express()
 const port = 5000
-
-const{User} = require("./models/User");
-const bodyParser = require('body-parser');
-
-
-//application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(bodyParser.json());
-//appplication/json
-
 
 const mongoose = require('mongoose')
 mongoose.connect('mongodb+srv://inkyeong:inkyeong@cluster0.msxuf.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',{
@@ -21,22 +10,6 @@ mongoose.connect('mongodb+srv://inkyeong:inkyeong@cluster0.msxuf.mongodb.net/myF
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
-})
-
-app.post('/register',(req,res) =>{
-  
-  //회원가입 할 때 필요한 정보들을 client에서 가져오면
-  //그것들을 데이터 베이스에 넣는다.
-  
-  const user = new User(req.body)
-  //body-parser가 있어서 request body로 받을 수 있다.
-
-  user.save((err,userInfo)=>{
-    if(err) return res.json({success: false,err})
-    return res.status(200).json({
-      success: true
-    })
-  })
 })
 
 app.listen(port, () => {
