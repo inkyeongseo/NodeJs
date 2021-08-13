450a4a852efdaa98a507989048a87518c44a70b4
inkyeong <dlsrud0415@naver.com>
2021-08-13 16:34:25 +0900

    nodejs

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const port = 5000
 const{User} = require("./models/User");
 const bodyParser = require('body-parser');
 
+const config = require('./config/key')
 
 //application/x-www-form-urlencoded   
 app.use(bodyParser.urlencoded({extended: true}));
@@ -14,13 +15,13 @@ app.use(bodyParser.json());
 
 
 const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://inkyeong:inkyeong@cluster0.msxuf.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',{
+mongoose.connect(config.mongoURI,{
   useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify:false
 }).then(() => console.log('MongoDB Connected...'))
   .catch(err => console.log(err))
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Hello World! 변경 테스트')
 })
 
 app.post('/register',(req,res) =>{
