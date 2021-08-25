3f349019bf1ae865c832ee077e2ec0218db9b2bf
inkyeong <dlsrud0415@naver.com>
2021-08-25 23:02:05 +0900

    ReactJs_Redux

---
+++ client/src/_reducer/index.js
@@ -0,0 +1,9 @@
+import { combineReducers } from "redux";
+//combineReudcers는 여러가지 reducer들을 RootReducer에서 하나로 합쳐주는 기능
+import user from './user_reducer';
+
+const rootReducer =  combineReducers({
+    //user,
+})
+
+export default rootReducer;
