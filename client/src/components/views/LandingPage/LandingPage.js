95635362310727f972e1d914d9c0d5a0d3a9b000
inkyeong <dlsrud0415@naver.com>
2021-09-04 23:31:41 +0900

    ReactJs_logout

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -1,12 +1,30 @@
+import axios from 'axios'
 import React from 'react'
 
-function LandingPage() {
+function LandingPage(props) {
+
+    const onClickHandler = () => {
+        axios.get('/api/users/logout')
+            .then(response=>{
+                //console.log(response.data)
+                if(response.data.success){
+                    props.history.push("/login")
+                }else{
+                    alert('로그아웃 실패')
+                }
+            })
+    }
+
     return (
         <div style={{
             display: 'flex', justifyContent : 'center', alignItems : 'center',
-            width: '100%', height : '100vh'
+            width: '100%', height : '100vh',flexDirection:'column'
         }}>
             <h2>시작페이지</h2>
+
+            <button onClick={onClickHandler}>
+                로그아웃
+            </button>
         </div>
     )
 }
